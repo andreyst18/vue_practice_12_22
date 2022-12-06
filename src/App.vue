95612a56af4c92e034fd7a0d4517114c3a012687
Andrey <andrejmax@yandex.ru>
2022-12-06 20:55:28 +0400

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
   <div id="app">
-    
+    <div class="wrapper">
+      <h2 class="title">Диваны</h2>
+      <ProductList/>
+    </div>
   </div>
 </template>
 
 <script>
+import ProductList from '@/components/ProductList.vue'
+
 export default {
   name: 'youtube-task',
+
+  components: {
+    ProductList
+  }
   
 };
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Heebo:wght@400;600&display=swap');
 
+*,
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Heebo', sans-serif;
+}
+
+.wrapper {
+  width: 900px;
+  margin: 50px auto;
+}
+
+.title {
+  margin-bottom: 20px;
+}
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="product-card">
+    <img 
+      :src="photo" 
+      class="product-card__img"
+    >
+    <div class="product-card__title">{{ title }}</div>
+    <div class="product-card__price">{{ price }}р.</div>
+    <div class="product-card__balance">Доступно {{ balance }} шт.</div>
+    <div>
+      <button 
+        class="product-card__btn"
+        @click="$emit('show-index', index)"
+      >Добавить в корзину</button>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'product-card',
+    props: {
+      title: String,
+      price: Number,
+      balance: {
+        type: Number,
+        required: true
+      }, 
+      photo: String,
+      index: Number
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.product-card {
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  border: 2px solid rgb(60, 71, 73, 0.2);
+  padding: 20px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  
+  &__img {
+    width: 300px;
+    align-self: center;
+  }
+
+  &__balance {
+    color: rgb(0, 0, 0, 0.4);
+    font-size: 12px;
+  }
+
+  &__title,
+ &__price,
+ &__balance {
+    font-weight: 600;
+    margin-bottom: 15px;
+  }
+
+  &__btn {
+    padding: 10px 20px;
+    border: none;
+    background: rgb(247, 68, 68);
+    border-radius: 10px;
+    color: #fff;
+    cursor: pointer;
+  }
+}
+
+</style>
