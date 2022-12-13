d49df49c9f2dfb13c07342b79882b2f51a94bf4b
Andrey <andrejmax@yandex.ru>
2022-12-13 19:50:28 +0400

    practice with form vue

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,136 @@
 <template>
   <div id="app">
     <div class="wrapper">
-      <div class="header">
-        <h2 class="title">Диваны</h2>
-        <my-input
-          @show-input="showInput"
-        />
-      </div>
-      
-      <ProductList/>
+      <form action="" class="col-lg-4 col-sm-4">
+        <div class="mb-4">
+          <label for="login" class="mb-2"><strong>Логин:</strong></label
+          ><br />
+          <input class="form-control" id="login" type="text" v-model="login"/>
+        </div>
+        <div class="mb-4">
+          <label for="email" class="mb-2"><strong>Почта:</strong></label
+          ><br />
+          <input class="form-control" id="email" type="text"  v-model="email"/>
+        </div>
+        <div class="mb-4">
+          <label for="password" class="mb-2"><strong>Пароль:</strong></label
+          ><br />
+          <input id="password" class="form-control" type="text"  v-model="password"/>
+        </div>
+        <div class="form-group mb-4">
+          <label for="country" class="mb-2">Страна:</label>
+          <select
+            id="country" 
+            class="form-control" 
+            v-model="country"
+          >
+            <option 
+              v-for="(item, index) in countries"
+              :key="index"
+              :value="item.value">
+              {{ item.label }}
+              </option>
+          </select>
+        </div>
+        <div class="mb-4">
+          <label for="themes" class="mb-2">Любимые темы:</label>
+          <br>
+          <select 
+            id="themes"
+            class="form-control" 
+            multiple
+            v-model="favoriteThemes"  
+          >
+            <option 
+              v-for="(item, index) in themes"
+              :key="index" 
+              :value="item.value">{{ item.label }}</option>
+          </select>
+        </div>
+        <div>
+          <label for="agree" class="me-3">Согласен с отправкой уведомлений:</label>
+          <input type="checkbox" id="agree" v-model="agreeWithSendEmail">
+        </div>
+        <div class="mt-4">
+          <h4>Любимые цвета:</h4>
+          <div class="mb-2">
+            <input class="me-2" type="checkbox" value="red" v-model="favoriteColors"><label for="">Красный</label>
+          </div>
+          <div class="mb-2">
+            <input class="me-2" type="checkbox" value="green" v-model="favoriteColors"><label for="">Зеленый</label>
+          </div>
+          <div class="mb-2">
+            <input class="me-2" type="checkbox" value="blue" v-model="favoriteColors"><label for="">Синий</label>
+          </div>
+        </div>
+        <div class="mt-4">
+          <h4>Укажите пол:</h4>
+        <div class="d-flex">
+          
+          <div class="me-3">
+            <label class="me-2" for="male">Мужчина</label>
+            <input id="male" type="radio" value="male" v-model="gender">
+          </div>
+          <div>
+            <label class="me-2" for="female">Женщина</label>
+            <input id="female" type="radio" value="female" v-model="gender">
+          </div>
+        </div>
+        </div>
+        
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-import ProductList from '@/components/ProductList.vue'
-import MyInput from '@/components/MyInput.vue'
-
 export default {
-  name: 'youtube-task',
-
-  components: {
-    ProductList,
-    MyInput
+  data() {
+    return {
+      login: "",
+      email: "",
+      password: "",
+      country: 'Russia',
+      agreeWithSendEmail: true,
+      favoriteThemes: [ 'Sport' ],
+      favoriteColors: [],
+      gender: '',
+      countries: [
+          {
+            label: 'Россия',
+            value: 'Russia'
+          },
+          {
+            label: 'Китай',
+            value: 'China'
+          },
+          {
+            label: 'Турция',
+            value: 'Turkey'
+          }
+        ],
+      themes: [
+        {
+          label: 'Технологии',
+          value: 'IT'
+        },
+        {
+          label: 'Языки',
+          value: 'Languages'
+        },
+        {
+          label: 'Спорт',
+          value: 'Sport'
+        },
+      ]
+    };
   },
-
-  methods: {
-    showInput(text) {
-      console.log(text)
-    }
-  }
-  
 };
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Heebo:wght@400;600&display=swap');
-
-*,
-body,
-html {
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: 'Heebo', sans-serif;
-}
-
 .wrapper {
-  width: 900px;
   margin: 50px auto;
-}
-
-.header {
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-}
-
-.title {
-  margin-right: 20px;
+  max-width: 900px;
 }
 </style>
